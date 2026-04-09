d0ac6fd74883de834e377c6879609052dc5407ae
AJIT KUMAR CHAUDHARY <azitchydev@gmail.com>
2026-04-09 10:38:12 +0545

    test

--- src/components/GradientIcon.tsx
+++ src/components/GradientIcon.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
-export function GradientIcon({ 
-  Icon, 
-  size = 24, 
+export function GradientIcon({
+  Icon,
+  size = 24,
   normalColor = "currentColor",
-  gradientColors = ['#A5DC53', '#5DD27A'], 
+  gradientColors = ["#A5DC53", "#5DD27A"],
   className = "",
-  ...props 
+  ...props
 }) {
   const gradientId = React.useId();
-  
+
   return (
-    <div className={`icon-with-gradient relative inline-block ${className}`} {...props}>
+    <div
+      className={`icon-with-gradient relative inline-block ${className}`}
+      {...props}
+    >
       <svg width="0" height="0" className="absolute">
         <defs>
           <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="100%">
@@ -20,19 +23,19 @@ export function GradientIcon({
           </linearGradient>
         </defs>
       </svg>
-      
-      <Icon 
-        size={size} 
+
+      <Icon
+        size={size}
         className="icon-normal absolute inset-0"
         style={{ color: normalColor }}
       />
-      
-      <Icon 
-        size={size} 
+
+      <Icon
+        size={size}
         className="icon-gradient"
-        style={{ 
+        style={{
           stroke: `url(#${gradientId})`,
-          fill: 'none'
+          fill: "none",
         }}
       />
     </div>
